4fa6b12cdc912377080d4f6dba9b36141fa3dae3
SangWon7242 <gotkdqja@gmail.com>
2025-09-10 21:49:34 +0900

    홈 컨트롤러 도입

---
+++ bang_gusuk_coder_blog/src/main/java/com/psw/bang_gusuk_coder_blog/boundedContext/home/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.psw.bang_gusuk_coder_blog.boundedContext.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+  @GetMapping("/")
+  public String showHome() {
+    return "/";
+  }
+}
+
